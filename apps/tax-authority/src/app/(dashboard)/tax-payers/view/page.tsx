3d3b729e9c851b4926c3f4907743c5dedce4ee29
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-02-01 22:59:16 +0100

    tax payers

---
+++ apps/tax-authority/src/app/(dashboard)/tax-payers/view/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ManageClient from "@/pattern/clients/templates/manage-client";
+
+const Manage = () => {
+	return (
+		<div className='w-full min-h-full h-fit flex flex-col gap-y-[144px] mb-[144px]'>
+			<ManageClient />
+		</div>
+	);
+};
+
+export default Manage;
