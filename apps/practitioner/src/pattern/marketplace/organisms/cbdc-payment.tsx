0714f9c304314a28594975e47dca48f67b551671
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-02-01 09:36:23 +0100

    payment information

---
+++ apps/practitioner/src/pattern/marketplace/organisms/cbdc-payment.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import DeleteIcon from "../atoms/delete-icon";
+import DeletePaymentModal from "../molecules/delete-bank-account";
+
+const CBDCPayment = () => {
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+	const closeDeleteModal = () => setIsDeleteModalOpen(false);
+	const openDeleteModal = () => setIsDeleteModalOpen(true);
+
+	// Mock data for orders
+	const payments = [
+		{
+			id: 1,
+			cbdc: "eNaira",
+			wallet_id: "1234567",
+			added: "May 1, 2023",
+		},
+		{
+			id: 2,
+			cbdc: "eNaira",
+			wallet_id: "1234567",
+			added: "May 1, 2023",
+		},
+	];
+
+	return (
+		<div className='p-6 '>
+			<div className='grid xl:grid-cols-3 gap-4'>
+				{payments.map((payment) => (
+					<div key={payment.id} className='bg-white  w-full p-4 rounded-md'>
+						<div className='bg-accent text-sm flex flex-col gap-4 p-4 rounded-md'>
+							<p>
+								<span className='text-grey-400'>CBDC: </span>
+								{payment.cbdc}
+							</p>
+							<p>
+								<span className='text-grey-400'>Wallet ID: </span>
+								{payment.wallet_id}
+							</p>
+						</div>
+						<div className='flex justify-between text-sm mt-4 w-full'>
+							<p>
+								<span className='text-grey-400'>Added: </span>
+								{payment.added}
+							</p>
+							<div
+								className='flex items-center gap-2 text-secondary cursor-pointer'
+								onClick={openDeleteModal}
+							>
+								<DeleteIcon />
+								<p>Delete</p>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+
+			<DeletePaymentModal
+				isOpen={isDeleteModalOpen}
+				onClose={closeDeleteModal}
+			/>
+		</div>
+	);
+};
+
+export default CBDCPayment;
---
+++ apps/practitioner/src/pattern/marketplace/templates/payment-info-tabs.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import MainMenuIcon from "../atoms/menu-icon";
+import { useRouter } from "next/navigation";
+import BankAccount from "../organisms/bank-accounts";
+import CryptoPayment from "../organisms/crypto-payment";
+import CBDCPayment from "../organisms/cbdc-payment";
+
+const PaymentInfoTabs = () => {
+	const [activeTab, setActiveTab] = useState("Bank accounts");
+	const router = useRouter();
+	const handleGoBack = () => {
+		router.back();
+	};
+
+	const renderContent = () => {
+		switch (activeTab) {
+			case "Bank accounts":
+				return (
+					<div className=' gap-4 w-full items-start mt-4'>
+						<BankAccount />
+					</div>
+				);
+			case "Crypto":
+				return (
+					<div className=' gap-4 w-full items-start mt-4'>
+						<CryptoPayment />
+					</div>
+				);
+
+			case "CBDC":
+				return (
+					<div className=' gap-4 w-full items-start mt-4'>
+						<CBDCPayment />
+					</div>
+				);
+
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div className='h-full w-full flex flex-col'>
+			{/* Fixed Tabs */}
+			<div className='fixed left-0 w-full bg-white shadow-md z-10 pt-3'>
+				<div className='flex flex-col justify-between md:px-8 mx-auto px-4'>
+					<div
+						className='flex items-center gap-2 cursor-pointer'
+						onClick={handleGoBack}
+					>
+						<MainMenuIcon />
+						<h6 className='font-bold fomt-sen'>Payment Information</h6>
+					</div>
+
+					<div className='flex'>
+						<button
+							className={`px-4 py-4 font-medium ${
+								activeTab === "Bank accounts"
+									? "border-b-2 border-[#D82E2E] text-[#202B3C] font-medium"
+									: "text-grey-300"
+							}`}
+							onClick={() => setActiveTab("Bank accounts")}
+						>
+							Bank Accounts
+						</button>
+						<button
+							className={`px-4 py-2 font-medium ${
+								activeTab === "Crypto"
+									? "border-b-2 border-[#D82E2E] text-[#202B3C] font-medium"
+									: "text-grey-300"
+							}`}
+							onClick={() => setActiveTab("Crypto")}
+						>
+							Crypto
+						</button>
+						<button
+							className={`px-4 py-2 font-medium ${
+								activeTab === "CBDC"
+									? "border-b-2 border-[#D82E2E] text-[#202B3C] font-medium"
+									: "text-grey-300"
+							}`}
+							onClick={() => setActiveTab("CBDC")}
+						>
+							CBDC
+						</button>
+					</div>
+				</div>
+			</div>
+
+			{/* Tab Content */}
+			<div className='pt-24 md:px-8 w-full'>{renderContent()}</div>
+		</div>
+	);
+};
+
+export default PaymentInfoTabs;
